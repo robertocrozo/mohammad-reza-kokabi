729f15dab6daad4da2320addaadfa00c6d5f1a33
robertocrozo <103831366+robertocrozo@users.noreply.github.com>
2024-08-14 04:23:45 +0330

    Add files via upload

---
+++ tel_8.py
@@ -0,0 +1,9 @@
+even_numbers = []
+odd_numbers = []
+for number in range(1, 101):
+    if number % 2 == 0:
+        even_numbers.append (number)
+    else:
+        odd_numbers.append (number)
+print("enen number = ", even_numbers)
+print("odd number = ", odd_numbers)
